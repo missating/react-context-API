966482e164da53f611162bf895c5fe3db6324b15
Vanessa Ating <atingenkay@gmail.com>
2019-10-17 02:14:21 +0100

    add validations to dynamically change input colors

--- src/GrandChild.js
+++ src/GrandChild.js
@@ -5,26 +5,52 @@ import "./styles.css";
 
 const GrandChild = () => (
   <Consumer>
-    {({ color, handleChange }) => (
-      <div class="container">
-        <form>
-          <div>
-            <h2 class="title">The Dynamic Form</h2>
-            <div className="row">
-              <input type="text" id="user-id" placeholder="User Id" />
+    {({
+      userId,
+      password,
+      userIdError,
+      passwordError,
+      ucolor,
+      pcolor,
+      onChange,
+      onLogin,
+      onCancel
+    }) => (
+        <div className="container">
+          <form>
+            <div>
+              <h2 className="title">The Dynamic Form</h2>
+              <div className="row">
+                <input
+                  type="text"
+                  id="user-id"
+                  name="userId"
+                  placeholder="User Id"
+                  onChange={onChange}
+                  value={userId}
+                  style={{ backgroundColor: ucolor }}
+                />
+              </div>
+              <div className="row">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={onChange}
+                  value={password}
+                  style={{ backgroundColor: pcolor }}
+                />
+              </div>
+              <div className="btn-row">
+                <button type="submit" onClick={onLogin} className="btn">Login</button>
+                <button type="submit" onClick={onCancel} className="btn">Cancel</button>
+              </div>
             </div>
-            <div className="row">
-              <input type="password" id="password" placeholder="Password" />
-            </div>
-            <div className="btn-row">
-              <button type="submit" className="btn">Login</button>
-              <button type="submit" className="btn">Cancel</button>
-            </div>
-          </div>
 
-        </form>
-      </div>
-    )}
+          </form>
+        </div>
+      )}
   </Consumer>
 );
 
--- src/index.js
+++ src/index.js
@@ -6,22 +6,43 @@ import Child from "./Child";
 import "./styles.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      // color: "yellow",
-      // handleChange: this.handleChange
+      userId: '',
+      password: '',
+      userIdError: '',
+      passwordError: '',
+      pcolor: '',
+      ucolor: '',
+      onChange: this.onChange,
+      onLogin: this.onLogin
     };
   }
 
-  // handleChange = event => {
-  //   event.preventDefault();
-  //   this.setColor(event.target.value);
-  // };
+  onChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-  // setColor = color => {
-  //   this.setState({ color });
-  // };
+  onLogin = event => {
+    event.preventDefault();
+    if (/^ *$/.test(this.state.userId)) {
+      this.setState({ ucolor: 'yellow', userIdError: 'user id warning error' })
+    }
+    if (/^ *$/.test(this.state.password)) {
+      this.setState({ pcolor: 'yellow', passwordError: 'password warning error' })
+    }
+    if (this.state.userId && this.state.userId.trim() !== 'testUser') {
+      this.setState({ ucolor: 'red', userIdError: 'user id danger error' })
+    }
+    if (this.state.password && this.state.password.trim() !== 'mypassword') {
+      this.setState({ pcolor: 'red', passwordError: 'password danger error' })
+    }
+  }
+
   render() {
     return (
       <Provider value={this.state}>
